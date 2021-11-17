15259105c2d6e7eb9fd3bd5664d60fdbbbe1cfa7
yuancf1024 <3455911441@qq.com>
2021-11-17 10:03:43 +0800

    最基本的密码安全判断实现

--- 2021Practical_Python_Programming/pass_gen.py
+++ 2021Practical_Python_Programming/pass_gen.py
@@ -1,7 +1,30 @@
 # TODO: 用户输入密码
 user_password = input("请输入新密码：")
 
-# TODO: 判断密码是否合格
+# TODO: 判断密码安全性
+UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LOWER = 'abcdefghijklmnopqrstuvwxyz'
+DIGIT = '0123456789'
+PUNCTUATION = '~!@#$%^&*()_+-+:";\'<>,./\\?'
 
+have_upper = False
+have_lower = False
+have_digit = False
+have_punctuation = False
+
+for char in user_password:
+    if char in UPPER:
+        have_upper = True
+    if char in LOWER:
+        have_lower = True
+    if char in DIGIT:
+        have_digit = True
+    if char in PUNCTUATION:
+        have_punctuation = True
+
+is_secure = (have_upper 
+    and have_lower 
+    and have_digit 
+    and have_punctuation)
 # TODO: 输出
-
+print(is_secure)
