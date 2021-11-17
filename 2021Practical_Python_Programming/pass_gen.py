7c13ab4cd4e0c6a65d614fa1a56c2d2a696a6a7e
yuancf1024 <3455911441@qq.com>
2021-11-17 11:48:16 +0800

    函数化改造

--- 2021Practical_Python_Programming/pass_gen.py
+++ 2021Practical_Python_Programming/pass_gen.py
@@ -1,14 +1,13 @@
 import string
+# TODO: 用户输入密码
+# TODO: 判断密码安全性
+# TODO: 输出结果
 
-while 1:
-    # TODO: 用户输入密码
-    user_password = input("请输入新密码：")
-
-    # TODO: 判断密码安全性
- 
+def evaluate_password(password):
+    result = False
     password_state = 0b00000
 
-    for char in user_password: # 下面的条件属于多选一，具有排他性
+    for char in password: # 下面的条件属于多选一，具有排他性
         if char in string.ascii_uppercase:
             password_state |= 0b10000
         elif char in string.ascii_lowercase:
@@ -18,14 +17,12 @@ while 1:
         else:
             password_state |= 0b00010
 
-    if len(user_password) >= 8:
+    if len(password) >= 8:
         password_state |= 0b00001
-
-    # TODO: 输出结果
 
     if password_state == 0b11111:
         print('密码符合要求！')
-        break
+        result = True
     else:
         prompt = '密码不符合要求，' # 下面的条件属于并行判断
         if password_state & 0b00001 == 0: # 长度不足8时，按位与为0
@@ -40,3 +37,12 @@ while 1:
             prompt += '没包含标点，'
         prompt = prompt[:-1]
         print(prompt)
+    return result
+
+def main():
+    while 1:
+        user_password = input("请输入新密码：")
+        if evaluate_password(user_password):
+            break
+
+main()
